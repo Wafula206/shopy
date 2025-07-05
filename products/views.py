d2587beec877a55cdb48ea01928788239206be50
Wafula206 <wafulabrian206@gmail.com>
2025-07-05 20:43:20 +0300

    implemented a search functionality on product and styled

--- products/views.py
+++ products/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Product
 
 def product_list(request):
-    products = Product.objects.filter(is_active=True)
+    query = request.GET.get('q')
+    products = Product.objects.all()
+
+    if query:
+        products = products.filter(
+            Q(name__icontains=query) | Q(category__icontains=query)
+        )
+
     return render(request, 'products/product_list.html', {'products': products})
